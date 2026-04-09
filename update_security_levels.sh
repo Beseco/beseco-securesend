f04ae4f69961a33388f5fffc287aca0513919b58
Beseco <florian@beubl.com>
2026-04-09 19:04:27 +0200

    Fix: Simplified JSONB concatenation

--- update_security_levels.sh
+++ update_security_levels.sh
@@ -7,13 +7,9 @@ echo "Updating security levels for all organizations..."
 docker exec securesend_db psql -U securesend -d securesend -c "
 UPDATE organizations 
 SET settings_json = 
-  CASE 
-    WHEN settings_json IS NULL THEN '{\"allowed_security_levels\": [\"normal\", \"standard\", \"secure\", \"extended\", \"advanced\", \"maximal\"]}'::jsonb
-    WHEN settings_json->>'allowed_security_levels' NOT LIKE '%maximal%' THEN 
-      (settings_json || '{\"allowed_security_levels\": [\"normal\", \"standard\", \"secure\", \"extended\", \"advanced\", \"maximal\"]}'::jsonb)::jsonb
-    ELSE settings_json
-  END
-WHERE settings_json IS NOT NULL;
+  '{\"allowed_security_levels\": [\"normal\", \"standard\", \"secure\", \"extended\", \"advanced\", \"maximal\"]}'::jsonb
+WHERE settings_json IS NOT NULL 
+AND settings_json->>'allowed_security_levels' NOT LIKE '%maximal%';
 "
 
 echo "Done!"
